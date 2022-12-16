85423984e9bb986e7db32b3dcb3bcad9747f568a
pamboss13 <pambossvr@gmail.com>
2022-12-16 22:03:49 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+
+app.use(function(req,res,next){
+    console.log('Request date'+ new Date());
+    //res.sendFile("./static/cool.txt");
+    next();
+})
+
+app.use(function(req,res,next){
+    var filepath = path.join(__dirname,"static",req.url);
+    fs.stat(filepath,function(err,fileinfo){
+        if(err){
+            next();
+            return
+        }
+        if(fileinfo.isFile()){
+            res.sendFile(filepath);
+        }else{
+            next()
+        }
+    })
+})
+
+app.use(function(req,res){
+    res.status(404);
+    res.send('File Not Found!')
+})
+
+app.listen(port, ()=>{
+    console.log('Listening on http://localhost:3000')
+});
